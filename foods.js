2281e8765c76b2b7507ef75f522a3e9099f983ab
AndersonBeltran <abeltran@appdev.yearup.org>
2024-10-22 15:39:01 -0400

    Organized

--- foods.js
+++ foods.js
@@ -9,10 +9,11 @@ let subtotal = 0;
 for (let food = 0; food < lunch.length; food++) {
   subtotal += lunch[food].price;
 }
-console.log("Subtotal: $" + subtotal.toFixed(2));
 let tax = subtotal * 0.08;
-console.log("Tax (8%): $" + tax.toFixed(2));
 let tip = subtotal * 0.18;
-console.log("Tip (18%): $" + tip.toFixed(2));
 let total = subtotal + tax + tip;
+
+console.log("Subtotal: $" + subtotal.toFixed(2));
+console.log("Tip (18%): $" + tip.toFixed(2));
+console.log("Tax (8%): $" + tax.toFixed(2));
 console.log("Total Due: $" + total.toFixed(2));
